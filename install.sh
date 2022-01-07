ec8930249b649be6aeb4ab29b4afed6df460b061
mathlet <mcdearmanchris@gmail.com>
2022-01-06 18:30:09 -0600

    add zsh syntax

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-sudo pacman -S zsh zsh-completions zsh-autosuggestions ttf-hack ttf-dejavu noto-fonts-cjk
+sudo pacman -S zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting ttf-hack ttf-dejavu noto-fonts-cjk
 
 cp -r -v config/* ~/.config
 cp -r -v home/.*rc ~/.
 
-chsh -l /usr/bin/zsh
+chsh -s /usr/bin/zsh
 
 
