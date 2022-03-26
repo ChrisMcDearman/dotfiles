1a1caa98715903c99d495e4c2008789a3ce31ada
mathlet <mcdearmanchris@gmail.com>
2022-03-26 16:33:31 -0500

    add back i3 config setup

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-sudo pacman -S zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S i3-gaps kitty rofi dunst feh zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
+yay -S polybar picom-ibhagwan-git 
 
 cp -r -v config/* ~/.config
 cp -r -v home/.*rc ~/.
 touch ~/.zsh_history
+cp -r -v wallpapers ~/Pictures/
 
 chsh -s /usr/bin/zsh
 
