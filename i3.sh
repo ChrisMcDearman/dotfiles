67cb0f0a2b5add632e59c8e84cdf9c6eaa4217e5
mathlet <mcdearmanchris@gmail.com>
2022-04-01 18:28:10 -0500

    separate i3

---
+++ i3.sh
@@ -0,0 +1,2 @@
+sudo pacman -S --needed i3-gaps kitty rofi dunst feh ttf-fira-code
+yay -S polybar picom-ibhagwan-git rofi-bluetooth-git 
--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sudo pacman -S --needed i3-gaps kitty rofi dunst feh zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting ttf-fira-code
-yay -S polybar picom-ibhagwan-git rofi-bluetooth-git 
+sudo pacman -S --needed  zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting ttf-fira-code
 
 
 
