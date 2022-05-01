d07b7fcfa5bfefa75aff67cd608800d166ddc84f
mathlet <mcdearmanchris@gmail.com>
2022-05-01 12:07:09 -0500

    add rust install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo pacman -S --needed  zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting ttf-fira-code
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 
