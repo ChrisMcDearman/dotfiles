8d885da4cf95bf5b0f36a083652838c35f0b81be
mathlet <mcdearmanchris@gmail.com>
2022-05-28 12:34:17 -0500

    add brightnessctl to i3 script

--- i3.sh
+++ i3.sh
@@ -1,2 +1,2 @@
-sudo pacman -S --needed i3-gaps kitty rofi dunst feh ttf-fira-code
+sudo pacman -S --needed i3-gaps kitty rofi dunst feh ttf-fira-code brightnessctl
 yay -S polybar picom-ibhagwan-git rofi-bluetooth-git 
