bc4a96d1c9bb95aa33e43f7013b8296a70e312cb
mathlet <mcdearmanchris@gmail.com>
2022-08-12 15:48:06 -0500

    update doom config

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -6,9 +6,6 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Chris McDearman"
-      user-mail-address "mcdearmanchris@gmail.com")
-
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
